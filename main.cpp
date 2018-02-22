31ba13a5f881534fdc7a22f8c9e23ef9e4845c5b
Rutger Klamer <rutger@rutgerklamer.nl>
2018-02-22 13:30:00 +0100

    Add printing multiple chars

--- main.cpp
+++ main.cpp
@@ -44,37 +44,57 @@ void runCode()
 	int i = 0;
 	for (i = 0; i < m_ruc_code.size(); i++) {
 		if (!m_false_loop) {
-			if (m_ruc_code[i].find("JMPR") != std::string::npos)
+			if (m_ruc_code[i].find("JMPR") != std::string::npos){
 				wrapPosition(getIntFromString(m_ruc_code[i]));
-			else if (m_ruc_code[i].find("JMPL") != std::string::npos)
+			} else if (m_ruc_code[i].find("JMPL") != std::string::npos){
 				wrapPosition(-getIntFromString(m_ruc_code[i]));
-			else if (m_ruc_code[i].find("ADD") != std::string::npos && m_ruc_code[i].find("ADDS") == std::string::npos)
+			} else if (m_ruc_code[i].find("ADD") != std::string::npos && m_ruc_code[i].find("ADDS") == std::string::npos){
 				wrapValue(getIntFromString(m_ruc_code[i]));
-			else if (m_ruc_code[i].find("SUB") != std::string::npos && m_ruc_code[i].find("SUBS") == std::string::npos)
+			} else if (m_ruc_code[i].find("SUB") != std::string::npos && m_ruc_code[i].find("SUBS") == std::string::npos){
 				wrapValue(-getIntFromString(m_ruc_code[i]));
-			else if (m_ruc_code[i].find("STLP") != std::string::npos)
+			} else if (m_ruc_code[i].find("STLP") != std::string::npos){
 				(m_cells[m_cell] != 0 ? m_current_loop.push_back(i) : m_false_loop = true, m_found_brackets = 0);
-			else if (m_ruc_code[i].find("NDLP") != std::string::npos)
+			} else if (m_ruc_code[i].find("NDLP") != std::string::npos){
 				((int)m_cells[m_cell] == 0 ? m_current_loop.pop_back() : i = m_current_loop[m_current_loop.size() - 1]);
-			else if (m_ruc_code[i].find("PRNTA") != std::string::npos)
-				std::cout << static_cast<char>(m_cells[m_cell]) << std::flush;
-			else if (m_ruc_code[i].find("PRNTD") != std::string::npos)
-				std::cout << (m_cells[m_cell]) << std::flush;
-			else if (m_ruc_code[i] == "CINP")
+			} else if (m_ruc_code[i].find("PRNTA") != std::string::npos){
+				if (getIntFromString(m_ruc_code[i]) > 1) {
+					for (int j = 0; j < getIntFromString(m_ruc_code[i]); j++) {
+						std::cout << static_cast<char>(m_cells[m_cell]) << std::flush;
+						wrapPosition(1);
+					}
+				}
+				else {
+					std::cout << static_cast<char>(m_cells[m_cell]) << std::flush;
+				}
+			} else if (m_ruc_code[i].find("PRNTD") != std::string::npos){
+				if (getIntFromString(m_ruc_code[i]) > 1) {
+					for (int j = 0; j < getIntFromString(m_ruc_code[i]); j++) {
+						std::cout << (m_cells[m_cell]) << std::flush;
+						wrapPosition(1);
+					}
+				}
+				else {
+					std::cout << (m_cells[m_cell]) << std::flush;
+				}
+			} else if (m_ruc_code[i] == "CINP"){
 				(m_inp_pos < m_inp.size() ? m_cells[m_cell] = static_cast<int>(m_inp[m_inp_pos]), m_inp_pos++ : m_cells[m_cell] = 0);
-			else if (m_ruc_code[i].find("PSHS") != std::string::npos)
+			} else if (m_ruc_code[i].find("PSHS") != std::string::npos){
 				m_stack.push_back(m_cells[m_cell]);
-			else if (m_ruc_code[i].find("CLRS") != std::string::npos)
+			} else if (m_ruc_code[i].find("CLRS") != std::string::npos){
 				m_stack.empty();
-			else if (m_ruc_code[i].find("RMVS") != std::string::npos) {
+			} else if (m_ruc_code[i].find("RMVS") != std::string::npos) {
 				if (m_stack.size() > 0)
 					m_stack.erase(m_stack.begin());
-			} else if (m_ruc_code[i].find("EQL TOPS") != std::string::npos)
+			} else if (m_ruc_code[i].find("EQL TOPS") != std::string::npos){
 				m_cells[m_cell] = m_stack[m_stack.size() - 1];
-			else if (m_ruc_code[i].find("ADDS") != std::string::npos)
+			} else if (m_ruc_code[i].find("ADDS") != std::string::npos){
 				addSubStack(m_stack[m_stack.size() - 1], m_stack[m_stack.size() - 2]);
-			else if (m_ruc_code[i].find("SUBS") != std::string::npos)
+			} else if (m_ruc_code[i].find("SUBS") != std::string::npos){
 				addSubStack(m_stack[m_stack.size() - 1], m_stack[m_stack.size() - 2]);
+			}
+			else if (m_ruc_code[i].find("HALT") != std::string::npos){
+				i = m_ruc_code.size();
+			}
 
 		}
 		else if (m_ruc_code[i].find("STLP") != std::string::npos) {
@@ -93,7 +113,7 @@ int getIntFromString(std::string s)
 		if ((int)s[i] >= 47 && (int)s[i] <= 57)
 			s.erase(s.begin(), s.begin() + i);
 		else if (i >= s.size() - 1)
-			return 0;
+			return 1;
 	}
 	return std::stoi(s);
 }
